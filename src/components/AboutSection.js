08b973f55fd0260b1b0007be4539118724657900
Wilson Huang <wilsonhuang@Wilsons-MacBook-Air-2.local>
2024-02-19 16:37:41 -0600

    homepage changes

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,7 +3,14 @@ import React from 'react';
 const aboutUs = () => {
     return (
         <div className="AboutUs"> 
-            <h> Hello this is the about section of our home screen! </h>
+            <div className="mini">
+                <p> A fake company website built for COSC4353 that will encompass our knowledge in Software Design by the end of this course! </p>
+            </div>
+            <div className="slogan"> 
+                <h1> Fueling Tomorrow, Sustaining Today: </h1>
+                <h1> Beginning the Journey to Net Zero. </h1>
+            </div>
+            <button className="LearnMore"> Learn More ↗</button>
         </div>
         
     );
