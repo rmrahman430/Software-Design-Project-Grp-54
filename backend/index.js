7a43d45b85a0129291ffd61b4a1391f39cd07ff1
wilsonhuangcs <willysonhuang@gmail.com>
2024-03-10 18:58:41 -0500

    login

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userModel = require('./models/userModel');
+const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(cors());
@@ -28,6 +29,35 @@ app.post('/register', async (req, res) => {
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
     console.error('Registration error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await userModel.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    if (user) {
+      const isMatch = await userModel.findOne({ password });
+      console.log("found", user);
+
+      if (isMatch) {
+        console.log("success");
+        res.json({ message: 'Login successful', user: { username: user.username, name: user.name }});
+      } else {
+        console.log("invalid pass");
+        res.status(400).json({ error: 'Invalid password' });
+      }
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Login error:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Form, Button, Card, Container } from "react-bootstrap";
-
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+  });
   
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setLoginData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(loginData),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Login successful:', data);
+        navigate('/profile'); 
+      } else {
+        console.error('Login failed:', await response.text());
+        const errorMessage = await response.json();
+        setError(errorMessage.error);
+      }
+    } catch (error) {
+      console.error('Login error:', error.message);
+      setError("Incorrect Username or Password.");
+
+    }
+  };
+
   return (
 
     <Container>
@@ -12,13 +53,15 @@ const LoginPage = () => {
         <Card style={{ width: '40rem' }}>
           <Card.Body>
             <div className="m-4">
-              <Form>
+              <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="formBasicUsername">
                   <div style={{display: 'flex', justifyContent: 'center'}}>
                     <Form.Control
                       type="text"
                       placeholder="Username"
                       name="username"
+                      value={loginData.username}
+                      onChange={handleChange}
                       required
                       style={{width: '200px'}}
                       className="form-control-sm"/>
@@ -36,12 +79,19 @@ const LoginPage = () => {
                   type="password" 
                   placeholder="Password" 
                   name="password"
+                  value={loginData.password}
+                  onChange={handleChange}
                   required
                   style={{width: '200px'}}
                   className="form-control-sm"/>
                 </div>
                 </Form.Group>
 
+                {error && (
+                  <div style={{ color: 'red', textAlign: 'center', marginBottom: '10px', fontSize: 'large' }}>
+                    {error}
+                  </div>
+                )}
 
                 <div style={{display: 'flex', justifyContent: 'center', paddingTop: "10px"}}>
                   <Button variant="primary" type="submit">
