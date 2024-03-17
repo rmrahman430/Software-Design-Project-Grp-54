2478e7904637296cef7d7832fdae707e5c1c8dd8
rmrahman430 <rayyanmrahman@gmail.com>
2024-03-17 17:37:25 -0500

    reverted middleware code

--- backend/Routes/AuthRoutes.js
+++ backend/Routes/AuthRoutes.js
@@ -3,7 +3,7 @@ const { checkUser } = require("../Middlewares/AuthMiddlewares");
 
 const router = require("express").Router();
 
-router.post("/profile", checkUser, profile);
+router.post("/profile", checkUser);
 router.post("/register", register);
 router.post("/login", login);
 
