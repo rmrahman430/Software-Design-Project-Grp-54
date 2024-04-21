0c5ece73bba5a1eee4155f4b7fc719abdcc207cb
rmrahman430 <rayyanmrahman@gmail.com>
2024-04-21 01:33:56 -0500

    code coverage report

--- backend/Coverage/AuthController.test.js
+++ backend/Coverage/AuthController.test.js
@@ -1,7 +1,6 @@
 // backend/tests/AuthController.test.js
 const request = require('supertest');
 const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
 const app = require('../index'); 
 
 let mongoServer;
